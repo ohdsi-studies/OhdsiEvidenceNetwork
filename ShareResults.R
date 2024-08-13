5ec016490e1dd2c8a2f5d9856ba7c798092abe84
Clair Blacketer <clair.blacketer@gmail.com>
2024-08-13 06:16:57 -0400

    Fixing issues in ShareResults.R

--- ShareResults.R
+++ ShareResults.R
@@ -1,22 +1,19 @@
 ##=========== START OF INPUTS ==========
 # The zip file created in the codeToRun.R. Copy the entire path after the phrase 
 # "Final results are now available in: " in the console.    
-zipFileName <- '[location of the zip file: e.g. /Documents/synthea/20220329/DbProfileResults.zip]'
+zipFileName <- "[location of the zip file: e.g. /Documents/synthea/20220329/DbProfileResults.zip]"
 # For uploading the results. You should have received the key file from the study coordinator:
 keyFileName <- "[location where you are storing: e.g. ~/keys/study-data-site-covid19.dat]"
-userName <- "data-site-datadiag"
-
 ##=========== END OF INPUTS ==========
 
 ##################################
 # DO NOT MODIFY BELOW THIS POINT
 ##################################
-outputLocation <- file.path(outputLocation, cdmSourceName, "strategusOutput")
-zipFile <- file.path(outputLocation, paste0(databaseName, ".zip"))
+userName <- "data-site-datadiag"
 
 OhdsiSharing::sftpUploadFile(
   privateKeyFileName = keyFileName, 
   userName = userName,
   remoteFolder = "/",
-  fileName = zipFile
+  fileName = zipFileName
 )
